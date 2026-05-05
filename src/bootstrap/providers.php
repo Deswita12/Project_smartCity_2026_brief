67ee170d3ab2d82c78b1c95586e069ede0f3578b
Deswita12 <desuwina@gmail.com>
2026-05-05 07:54:36 +0700

    add .gitignore and clean files

--- src/bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\Admin1PanelProvider::class,
-    App\Providers\Filament\OpdPanelProvider::class,
-    // App\Providers\Filament\OpdPanelProviderPanelProvider::class,
-];
